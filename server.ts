711d833773cee7203cc4449c2dfaefcb27f0c82a
aaronwlee <aaron.lee@cannalogue.ca>
2020-07-06 10:38:19 -0400

    fix(types): add types

--- server.ts
+++ server.ts
@@ -8,14 +8,4 @@ app.use(logger);
 app.use("/api", routers);
 app.error("/api", routers);
 
-app.get("/*", async (req, res) => {
-  console.log("2");
-  const { pathname } = req.url;
-  const isReactPath = pathname.split(".").length === 1;
-
-  if (isReactPath) {
-    await res.sendFile("./.attain/index.html");
-  }
-}, staticServe("./.attain", { maxAge: 1000 }));
-
 app.listen({ port: 3500 });
